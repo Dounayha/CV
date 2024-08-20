d67e018cbce10038ccadbea0c606777f54a1a7d8
Dounayha <dounayha05@gmail.com>
2024-08-20 18:39:45 +0200

    PROBLEME

--- index.php
+++ index.php
@@ -9,53 +9,82 @@
 </head>
 <body>
     <header></header>
-    <main class="container border g-0">
-    <div class="row align-items-start">
-        <!-- Colonne gauche -->
-        <div class="col-lg-4 col-6 g-0">
-            <div>
-                <img src="./assets/images/photoProfile.jpg" class="rounded-circle mx-auto d-block" width="50%" alt="photo de profil">
-            </div>
-
-            <!-- Contact -->
-            <div id="contact" class="px-5 mt-3">
-                <div class="fs-5 fw-bold mb-2">CONTACT</div>
-                <div class="mb-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone me-2" viewBox="0 0 16 16">
-                        <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58z"/>
-                    </svg>
-                    07 81 97 29 97
-                </div>
-                <div class="mb-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope me-2" viewBox="0 0 16 16">
-                        <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
-                    </svg> 
-                    dounayha.abdillah@gmail.com
-                </div>
+    <main class="container border">
+        <div class="row align-items-start">
+            <!-- Colonne gauche -->
+            <div class="col-lg-4 col-md-5 col-sm-12">
                 <div>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo me-2" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M8 1a3 3 0 1 0 0 6 3 3 0 0 0 0-6M4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999zm2.493 8.574a.5.5 0 0 1-.411.575c-.712.118-1.28.295-1.655.493a1.3 1.3 0 0 0-.37.265.3.3 0 0 0-.057.09V14l.002.008.016.033a.6.6 0 0 0 .145.15c.165.13.435.27.813.395.751.25 1.82.414 3.024.414s2.273-.163 3.024-.414c.378-.126.648-.265.813-.395a.6.6 0 0 0 .146-.15l.015-.033L12 14v-.004a.3.3 0 0 0-.057-.09 1.3 1.3 0 0 0-.37-.264c-.376-.198-.943-.375-1.655-.493a.5.5 0 1 1 .164-.986c.77.127 1.452.328 1.957.594C12.5 13 13 13.4 13 14c0 .426-.26.752-.544.977-.29.228-.68.413-1.116.558-.878.293-2.059.465-3.34.465s-2.462-.172-3.34-.465c-.436-.145-.826-.33-1.116-.558C3.26 14.752 3 14.426 3 14c0-.599.5-1 .961-1.243.505-.266 1.187-.467 1.957-.594a.5.5 0 0 1 .575.411"/>
-                    </svg>
-                    Marseille
+                    <img src="./assets/images/photoProfile.jpg" class="rounded-circle mx-auto d-block" width="50%" alt="photo de profil">
                 </div>
+
+                <!-- Contact -->
+                <div id="contact" class="px-5 mt-3">
+                    <div class="fs-5 fw-bold mb-2">CONTACT</div>
+                    <div class="mb-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone me-2" viewBox="0 0 16 16">
+                            <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58z"/>
+                        </svg>
+                        07 81 97 29 97
+                    </div>
+                    <div class="mb-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope me-2" viewBox="0 0 16 16">
+                            <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
+                        </svg> 
+                        dounayha.abdillah@gmail.com
+                    </div>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo me-2" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M8 1a3 3 0 1 0 0 6 3 3 0 0 0 0-6M4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999zm2.493 8.574a.5.5 0 0 1-.411.575c-.712.118-1.28.295-1.655.493a1.3 1.3 0 0 0-.37.265.3.3 0 0 0-.057.09V14l.002.008.016.033a.6.6 0 0 0 .145.15c.165.13.435.27.813.395.751.25 1.82.414 3.024.414s2.273-.163 3.024-.414c.378-.126.648-.265.813-.395a.6.6 0 0 0 .146-.15l.015-.033L12 14v-.004a.3.3 0 0 0-.057-.09 1.3 1.3 0 0 0-.37-.264c-.376-.198-.943-.375-1.655-.493a.5.5 0 1 1 .164-.986c.77.127 1.452.328 1.957.594C12.5 13 13 13.4 13 14c0 .426-.26.752-.544.977-.29.228-.68.413-1.116.558-.878.293-2.059.465-3.34.465s-2.462-.172-3.34-.465c-.436-.145-.826-.33-1.116-.558C3.26 14.752 3 14.426 3 14c0-.599.5-1 .961-1.243.505-.266 1.187-.467 1.957-.594a.5.5 0 0 1 .575.411"/>
+                        </svg>
+                        Marseille
+                    </div>
+                </div>
+
+                <!-- Profil -->
+                <div id="profil" class="px-5 mt-3">
+                    <div class="fs-5 fw-bold mb-2">PROFIL</div>
+                    <div>XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX</div>
+                </div>
+                <!-- COMPETENCE -->
+                <div id="competences" class="px-5 mt-3">
+                <div class="fs-5 fw-bold">COMPETENCES</div>
+                <div class="row">
+                <div class="col-4">HTML</div>
+                <div class="col-8"><div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar" style="width: 90%"></div>
+                </div>
+
+                
+                </div>
+                </div>
+                </div>
+                
+                
+
+                        
+                        
+                            
+                    </div>
+                </div>
+
+                <!-- centre d'interet -->
+                <div id="loisirs" class="px-5 mt-3"></div>
+                <div class="fs-5 fw-bold mb-2">INTERETS ET LOISIRS</div>
+                <ul>
+                    <li>Lecture</li>
+                    <li>Cuisine</li>  
+                </ul>
+
             </div>
 
-            <!-- Profil -->
-            <div id="profil" class="px-5 mt-3">
-            <div class="fs-5 fw-bold mb-2">PROFIL</div>
-            <div>XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX</div>
+            <!-- Colonne droite -->
+            <div class="col-8  p_colonne_droite">
+                <!-- Contenu de la colonne droite -->
+                One of three columns
+               
             </div>
         </div>
-
-        <!-- Colonne droite -->
-        <div class="col-lg-8 col-6 g-0 p_colonne_droite">
-            <!-- Contenu de la colonne droite -->
-            One of three columns
-        </div>
-    </div>
-</main>
-
-  
+    </main>
 
     <footer></footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>

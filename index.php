608842deba89b8b0d79c582c11cf8074c44e1855
Dounayha <dounayha05@gmail.com>
2024-08-20 11:51:52 +0200

    debut colonne + couleur

--- index.php
+++ index.php
@@ -3,12 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Mon Cv</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<link href="./assets/main.css" rel="stylesheet"/>
 </head>
 <body>
-    <p>
-        test 
-        TEST2
-    </p>
+    <header></header>
+<main class="container border">
+    <div class="row align-items-start">
+        <div class="col-md-4">
+            One of three columns
+        </div>
+        <div class="col-md-8 p_colonne_droite">
+            One of three columns
+        </div>
+    </div>
+</main>
+
+  
+
+    <footer></footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>

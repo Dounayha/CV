2e65f57720701d9a0194c6b5fc7b9825e0602d58
Dounayha <dounayha05@gmail.com>
2024-08-21 11:37:48 +0200

    finition couleur + a propos de moi

--- index.php
+++ index.php
@@ -45,14 +45,22 @@
                         </svg>
                         <span>Marseille</span>
                     </div>
+                    
+                    <div class="d-flex align-items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-car-front" viewBox="0 0 16 16">
+                         <path d="M4 9a1 1 0 1 1-2 0 1 1 0 0 1 2 0m10 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0M6 8a1 1 0 0 0 0 2h4a1 1 0 1 0 0-2zM4.862 4.276 3.906 6.19a.51.51 0 0 0 .497.731c.91-.073 2.35-.17 3.597-.17s2.688.097 3.597.17a.51.51 0 0 0 .497-.731l-.956-1.913A.5.5 0 0 0 10.691 4H5.309a.5.5 0 0 0-.447.276"/>
+                        <path d="M2.52 3.515A2.5 2.5 0 0 1 4.82 2h6.362c1 0 1.904.596 2.298 1.515l.792 1.848c.075.175.21.319.38.404.5.25.855.715.965 1.262l.335 1.679q.05.242.049.49v.413c0 .814-.39 1.543-1 1.997V13.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1.338c-1.292.048-2.745.088-4 .088s-2.708-.04-4-.088V13.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1.892c-.61-.454-1-1.183-1-1.997v-.413a2.5 2.5 0 0 1 .049-.49l.335-1.68c.11-.546.465-1.012.964-1.261a.8.8 0 0 0 .381-.404l.792-1.848ZM4.82 3a1.5 1.5 0 0 0-1.379.91l-.792 1.847a1.8 1.8 0 0 1-.853.904.8.8 0 0 0-.43.564L1.03 8.904a1.5 1.5 0 0 0-.03.294v.413c0 .796.62 1.448 1.408 1.484 1.555.07 3.786.155 5.592.155s4.037-.084 5.592-.155A1.48 1.48 0 0 0 15 9.611v-.413q0-.148-.03-.294l-.335-1.68a.8.8 0 0 0-.43-.563 1.8 1.8 0 0 1-.853-.904l-.792-1.848A1.5 1.5 0 0 0 11.18 3z"/>
+                        </svg> 
+                        <span>Permis B</span>
+
+                    </div>
                 </div>
 
                 <!-- Profil -->
                 <div id="profil" class="mt-4 text-start">
                     <h5 class="fw-bold ">PROFIL</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae eros eget tellus tristique
-                        bibendum.</p>
-                    <p>Suspendisse potenti. In faucibus massa arcu, vitae cursus mi hendrerit .</p>
+                    <p>Développeuse web passionnée par l'informatique, je suis à la recherche d'un emploi dans le domaine pour mettre en pratique mes compétences et contribuer à des projets innovants .</p>
+                    
                 </div>
 
                 <!-- Compétences -->
@@ -98,7 +106,7 @@
                     <h1 class="fw-bold ">Dounayha Abdillah</h1>
                     <h2 class="text-secondary">Développeur Web</h2>
                 </div>
-                <section id="experiences" class="bg-white fs-5 mt-3">
+                <section id="experiences" class="bg-white fs-5 mt-3 section-padding">
                     <h5 class="fw-bold ">EXPÉRIENCES PROFESSIONNELLES</h5>
                     <ul class="list-unstyled">
                         <li class="mb-2">Téléconseillère à pro BTP 
@@ -112,10 +120,10 @@
                         </li>
                     </ul>
                 </section>
-                <section id="formation" class="bg-white fs-5 mt-3">
+                <section id="formation" class="bg-white fs-5 mt-3 section-padding">
                     <h5 class="fw-bold ">FORMATIONS</h5>
                     <ul class="list-unstyled">
-                        <li class="mb-2">Titre professionnel développeur web
+                        <li class="mb-2">Titre professionnel développeur web et web mobile
                         <p class="text-muted small"> La plateforme 2024</p>
                         </li>
                         <li class="mb-2">BTS technico-commercial
@@ -129,9 +137,12 @@
             </div>
         </div>
     </main>
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    
     </script>
+
 </body>
 
 </html>

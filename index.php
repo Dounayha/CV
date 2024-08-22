bec920340a0187f2adae3d1958478346b12f11ae
Dounayha <dounayha05@gmail.com>
2024-08-22 15:45:32 +0200

    cv final

--- index.php
+++ index.php
@@ -136,12 +136,23 @@
                 </section>
             </div>
         </div>
-    </main>
-    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    
+
+        <button class="btn btn-secondary" onclick="myFunction()">
+            <span class="glyphicon glyphicon-print" aria-hidden="true"></span> Imprimer
+        </button>
+   
+
+    <script>
+        function myFunction() {
+            window.print();
+        }
     </script>
+    
+    </main>
+
+    
+       
+    
 
 </body>
 
